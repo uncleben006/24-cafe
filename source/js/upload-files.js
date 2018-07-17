ad8a52a30b25e11099fe53a2840c572834fd5930
王柏元 <uncleben006@gmail.com>
2018-07-17 18:11:31 +0800

    update admin page

--- source/js/upload-files.js
+++ source/js/upload-files.js
@@ -1,13 +1,23 @@
-function previewImages() {
+// Points to the root reference
+var storageRef = firebase.storage().ref();
+// Points to 'images'
+var imagesRef = storageRef.child('images');
+
+
+function previewImages(e) {
   var preview = document.querySelector('#preview')       
   let fileName = ''     
+  
   if (this.files) {
     for (let i = 0; i < this.files.length; i++) {
       fileName = fileName + this.files[i].name + ', '
       document.querySelector('.custom-file-input + label').innerHTML = fileName
+      // Get files and do upload image as promise function
+      var imageFile = e.target.files[i];
+      uploadImageAsPromise(imageFile);
     }          
     [].forEach.call(this.files, readAndPreview)
-  }
+  }  
   function readAndPreview(file) {
     // Make sure `file.name` matches our extensions criteria
     if (!/\.(jpe?g|png|gif)$/i.test(file.name)) {
@@ -15,16 +25,46 @@ function previewImages() {
     } // else...
     var reader = new FileReader()
     preview.innerHTML = ''
+    
     reader.addEventListener("load", function () {
-      var image = new Image()
-      image.height = 100
-      image.title = file.name
-      image.src = this.result
-      preview.appendChild(image)
-      // console.log('load')
-      // console.log(reader)
+      // var image = new Image()
+      var str = document.createElement('div')
+      str.className = 'shadow img-box'
+      str.innerHTML = 
+        ' <div class="text-center hover-delete flex-auto" onclick="this.parentElement.remove()">X</div>\
+          <div>\
+            <img class="img-fluid" src="'+ this.result +'">\
+          </div>'
+      preview.appendChild(str)
+      console.log(file)
+      console.log(reader)
     }, false)
     reader.readAsDataURL(file)
   }
 }
+//Handle waiting to upload each file using promise
+function uploadImageAsPromise (imageFile) {
+  return new Promise(function (resolve, reject) {
+      var storageRef = firebase.storage().ref("../images/"+imageFile.name);
+
+      //Upload file
+      var task = storageRef.put(imageFile);
+
+      //Update progress bar
+      task.on('state_changed',
+          function progress(snapshot){
+              var percentage = snapshot.bytesTransferred / snapshot.totalBytes * 100;
+              uploader.value = percentage;
+          },
+          function error(err){
+
+          },
+          function complete(){
+              var downloadURL = task.snapshot.downloadURL;
+          }
+      );
+  });
+}
+
+
 document.querySelector('.custom-file-input').addEventListener("change", previewImages, false)
